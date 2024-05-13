ba3abdb287082143c4f889cc0b4e4650bf1d53ca
PhungQuan-business <phunghongquan.business@gmail.com>
2024-05-13 16:26:41 +0700

    minor changes

--- src/algorithm/gen_requests.py
+++ src/algorithm/gen_requests.py
@@ -8,8 +8,8 @@ import json
 # Function to make request with player_ids batch
 def make_request_batch(player_ids_batch):
     # url = 'http://192.168.49.2:30796/process_batch'
-    url = 'http://127.0.0.1:5000/recommend'
-    # url = 'http://localhost:5000/recommend'
+    # url = 'http://127.0.0.1:5000/recommend'
+    url = 'http://localhost:5000/recommend'
     # url = 'https://7463-222-252-51-118.ngrok-free.app/recommend'
     data = {'playersObjectId': player_ids_batch}
     headers = {'Content-Type': 'application/json'}
@@ -28,14 +28,14 @@ def read_player_ids(filename, batch_size, truncate=None):
         player_ids = [line.strip() for line in file]
         
         # If truncate is None, default to 1000
-        if truncate is None:
+        if truncate == 'all':
+            player_ids = player_ids
+        # If truncate is 'None', take all values in the input file
+        elif truncate == None:
             truncate = 100
-        # If truncate is 'all', take all values in the input file
-        elif truncate == 'all':
-            truncate = len(player_ids)
-        
-        # Truncate the list if necessary
-        player_ids = player_ids[:truncate]
+            player_ids = player_ids[:truncate]
+        else:
+            player_ids = player_ids[:truncate]
     
     # Shuffle the player IDs to randomize
     random.shuffle(player_ids)  
@@ -48,17 +48,17 @@ def read_player_ids(filename, batch_size, truncate=None):
 # Main function
 def main():
     filename =  r'./objectid_v2-2.txt'  # File containing player IDs, one per line
-    batch_size = 1 # Batch size for each request
-    truncate = 5
+    batch_size = 2 # Batch size for each request
+    truncate = 10
     max_workers = 3
     
-    # tạo ra các batch người chơi
-    player_ids_batches = read_player_ids(filename, batch_size, truncate=truncate)
-    # data = {'playersObjectId': player_ids_batches}
-    # json_data = json.dumps(data)
+    # tạo batch người chơi
+    player_ids_batches = read_player_ids(filename=filename, 
+                                        batch_size=batch_size, 
+                                        truncate=truncate)
+    time_for_each_batch = []
     for i, batch in enumerate(player_ids_batches):
         start_time = time.time()   
-        # make_request_batch(player_ids_batch=player_ids_batches)
         make_request_batch(player_ids_batch=batch)
         # with ThreadPoolExecutor(max_workers=max_workers) as executor:
         #     try:
@@ -69,9 +69,9 @@ def main():
         #         sys.exit(1)
         end_time = time.time()
         time_took = end_time - start_time
+        time_for_each_batch.append(time_took)
         # print(f'Time it took to process all requests with parallel processing: {time_took} seconds')
-        print(f'Time it took to process batch with parallel processing: {time_took} seconds')
-    # print(json_data 
-
+        print(f'Time it took to process batch {i} without parallel processing is: {time_took} seconds')
+    print(f'total processing {len(time_for_each_batch)} batch(es) is:', sum(time_for_each_batch))
 if __name__ == "__main__":
     main()
